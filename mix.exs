78c9234315ea5b0ff37bf21eb5908ed0eb4eb89c
Rafael Iga <rafael.f.iga@gmail.com>
2020-06-05 12:17:49 -0300

    fix: deps

--- mix.exs
+++ mix.exs
@@ -19,9 +19,9 @@ defmodule EctoSearchy.MixProject do
 
   defp deps do
     [
-      {:ecto, "~> 3.4"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, "~> 0.15.5"}
+      {:ecto, ">= 3.0.0"},
+      {:ecto_sql, ">= 3.0.0"},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 end
