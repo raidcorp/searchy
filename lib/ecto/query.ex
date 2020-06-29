f3e17f7fbf14e2dfeb30affbc6774ec0876af185
thiagomajesk <thiagomgoulart@gmail.com>
2020-06-29 17:02:00 -0300

    Add aditional Postgres functions

--- lib/ecto/query.ex
+++ lib/ecto/query.ex
@@ -11,4 +11,49 @@ defmodule Searchy.Ecto.Query do
       )
     )
   end
+
+  def plainto_tsquery(search_field, search_term) do
+    dynamic(
+      [x],
+      fragment(
+        "? @@ plainto_tsquery(?)",
+        field(x, ^search_field),
+        ^search_term
+      )
+    )
+  end
+
+  def ts_rank(search_field, search_term) do
+    dynamic(
+      [x],
+      fragment(
+        "? @@ ts_rank(?)",
+        field(x, ^search_field),
+        ^search_term
+      )
+    )
+  end
+
+  def ts_rank_cd(search_field, search_term) do
+    dynamic(
+      [x],
+      fragment(
+        "? @@ ts_rank_cd(?)",
+        field(x, ^search_field),
+        ^search_term
+      )
+    )
+  end
+
+  def ts_headline(search_field, search_term) do
+    dynamic(
+      [x],
+      fragment(
+        "? @@ ts_headline(?)",
+        field(x, ^search_field),
+        ^search_term
+      )
+    )
+  end
+
 end
